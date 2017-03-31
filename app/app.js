f9f7b8a0b0c0f0db9d81a2f9d8881cb5dff8505b
Harry Epstein <harryeps@gmail.com>
2017-03-31 14:26:06 -0500

    stuff

---
+++ app/app.js
@@ -0,0 +1,64 @@
+"use strict";
+
+var app = angular.module("SleepDataTracker", ["ngRoute"]);
+
+app.config( function($routeProvider) {
+  $routeProvider
+  // START AT THE LANDING PAGE
+  .when('/', {
+    templateUrl: './partials/landingPage.html',
+    controller: 'LandingPagesCtrl'
+  })
+
+  .when('/eveningDiary', {
+    templateUrl: './partials/eveningDiary.html',
+    controller: 'EveningDiaryCtrl'
+  })
+
+  .when('/morningDiary', {
+    templateUrl: './partials/morningDiary.html',
+    controller: 'MorningDiaryCtrl'
+  })
+
+
+  .when('/getData', {
+    templateUrl: './partials/sleepData.html',
+    controller: 'retrieveDataCtrl'
+  })
+
+  .when('/login', {
+    templateUrl: './partials/login.html',
+    controller: 'UserCtrl'
+  })
+
+  .when('/sleepDataItem/morning/:sleepDataItem/edit', {
+    templateUrl: './partials/morningDiary.html',
+    controller: 'editMorningCtrl'
+  })
+
+  .when('/landingPage', {
+    templateUrl: './partials/landingPage.html',
+    controller: 'LandingPagesCtrl'
+  })
+
+  .when('/sleepDataItem/evening/:sleepDataItem/edit', {
+    templateUrl: './partials/eveningDiary.html',
+    controller: 'editEveningCtrl'
+  })
+  .otherwise({ redirectTo: '/' });
+});
+
+
+
+
+
+
+
+app.run(($location, FBCreds) => {
+    let creds = FBCreds;
+    let authConfig = {
+        apiKey: creds.apiKey,
+        authDomain: creds.authDomain
+    };
+    firebase.initializeApp(authConfig);
+});
